1cd5ce4c9f9a6dd44593bff80278f5401c9e6108
Arend Lammertink <lamare@gmail.com>
2024-12-26 16:59:36 +0100

    Ported existing NM to mps. Working now, needs clean up.

---
+++ nelder_mead.py
@@ -0,0 +1,196 @@
+import numpy as np
+from mpmath import mp, mpf
+
+
+# based on: https://github.com/owruby/nelder_mead
+
+class NelderMead(object):
+
+    def __init__(self, func, params, *args, **kwargs):
+        """ the Nelder-Mead method
+
+        :param func: objective function object
+        :param params: dict, tuning parameter, name: (min, max)
+        """
+        self.func = func
+        self.dim = len(params)
+        self.n_eval = 0
+        self.names = []
+        self.p_types = []
+        self.p_min = []
+        self.p_max = []
+        self.simplex = []
+        self.initialized = False
+
+        self._parse_minmax(params)
+
+    def initialize(self, init_params):
+        """ Initialize first simplex point
+
+        :param init_params(list):
+        """
+        assert len(init_params) == (self.dim + 1), "Invalid length of init_params"
+        for param in init_params:
+            p = Point(self.dim)
+            p.x = [mpf(val) for val in param]  # Use mpf for arbitrary precision
+            self.simplex.append(p)
+        self.initialized = True
+
+    def maximize(self, n_iter=20, delta_r=1, delta_e=2, delta_ic=-0.5, delta_oc=0.5, gamma_s=0.5):
+        """ Maximize the objective function. """
+        self._coef = -1
+        variables = locals()
+        for k, v in variables.items():
+            setattr(self, k, v)
+        self._opt(n_iter)
+
+    def minimize(self, n_iter=20, delta_r=1, delta_e=2, delta_ic=-0.5, delta_oc=0.5, gamma_s=0.5):
+        """ Minimize the objective function. """
+        self._coef = 1
+        variables = locals()
+        for k, v in variables.items():
+            setattr(self, k, v)
+        self._opt(n_iter)
+
+    def func_impl(self, x):
+        objval, invalid = None, False
+        for i, t in enumerate(x):
+            if t < self.p_min[i] or t > self.p_max[i]:
+                objval = float("inf")
+                invalid = True
+        if not invalid:
+            x = [int(round(x_t)) if p_t == "integer" else x_t for p_t, x_t in zip(self.p_types, x)]
+            objval = self._coef * self.func(x)
+        '''
+        print("{:5d} | {} | {:>15.5f}".format(
+            self.n_eval,
+            " | ".join([f"{float(t):15.5f}" for t in x]),
+            self._coef * float(objval)
+        ))
+        '''
+        self.n_eval += 1
+        return objval
+
+    def _opt(self, n_iter):
+        # Print Header
+        '''
+        print("{:>5} | {} | {:>15}".format(
+            "Eval",
+            " | ".join([f"{name:>15}" for name in self.names]),
+            "ObjVal"
+        ))
+        print("-" * (20 + self.dim * 20))
+        '''
+
+        if not self.initialized:
+            self._initialize()
+        for p in self.simplex:
+            p.f = self.func_impl(p.x)
+
+        for i in range(n_iter):
+            self.simplex = sorted(self.simplex, key=lambda p: p.f)
+
+            # centroid
+            p_c = self._centroid()
+            # reflect
+            p_r = self._reflect(p_c)
+
+            if p_r < self.simplex[0]:
+                p_e = self._expand(p_c)
+                if p_e < p_r:
+                    self.simplex[-1] = p_e
+                else:
+                    self.simplex[-1] = p_r
+                continue
+            elif p_r > self.simplex[-2]:
+                if p_r <= self.simplex[-1]:
+                    p_cont = self._outside(p_c)
+                    if p_cont < p_r:
+                        self.simplex[-1] = p_cont
+                        continue
+                    self.simplex[-1] = p_r
+                elif p_r > self.simplex[-1]:
+                    p_cont = self._inside(p_c)
+                    if p_cont < self.simplex[-1]:
+                        self.simplex[-1] = p_cont
+                        continue
+
+                # Shrink
+                for j in range(len(self.simplex) - 1):
+                    p = Point(self.dim)
+                    p.x = [p_c.x[i] + self.gamma_s * (self.simplex[j + 1].x[i] - p_c.x[i]) for i in range(self.dim)]
+                    p.f = self.func_impl(p.x)
+                    self.simplex[j + 1] = p
+            else:
+                self.simplex[-1] = p_r
+
+        self.simplex = sorted(self.simplex, key=lambda p: p.f)
+        print("\nBest Point: {}".format(self.simplex[0]))
+
+    def _centroid(self):
+        p_c = Point(self.dim)
+        x_sum = [p.x for p in self.simplex[:-1]]
+        p_c.x = [sum(x[i] for x in x_sum) / len(x_sum) for i in range(self.dim)]  # Use arbitrary precision
+        return p_c
+
+    def _reflect(self, p_c):
+        return self._generate_point(p_c, self.delta_r)
+
+    def _expand(self, p_c):
+        return self._generate_point(p_c, self.delta_e)
+
+    def _inside(self, p_c):
+        return self._generate_point(p_c, self.delta_ic)
+
+    def _outside(self, p_c):
+        return self._generate_point(p_c, self.delta_oc)
+
+    def _generate_point(self, p_c, x_coef):
+        p = Point(self.dim)
+        p.x = [p_c.x[i] + x_coef * (p_c.x[i] - self.simplex[-1].x[i]) for i in range(self.dim)]
+        p.f = self.func_impl(p.x)
+        return p
+
+    def _parse_minmax(self, params):
+        types = ["real", "integer","mpf"]
+        for name, values in params.items():
+            assert values[0] in types, "Invalid param type, Please check it."
+
+            self.names.append(name)
+            self.p_types.append(values[0])
+            self.p_min.append(values[1][0])
+            self.p_max.append(values[1][1])
+
+    def _initialize(self):
+        for i in range(self.dim + 1):
+            p = Point(self.dim)
+            init_val = [mpf((m2 - m1) * np.random.random() + m1) for m1, m2 in zip(self.p_min, self.p_max)]
+            p.x = init_val
+            self.simplex.append(p)
+
+
+class Point(object):
+
+    def __init__(self, dim):
+        self.x = [mpf(0) for _ in range(dim)]  # Use mpf for arbitrary precision
+        self.f = mpf(0)
+
+    def __str__(self):
+        return "Params: {}, ObjValue: {}".format(
+            ", ".join([f"{float(x):10.5f}" for x in self.x]),
+            float(self.f))
+
+    def __eq__(self, rhs):
+        return self.f == rhs.f
+
+    def __lt__(self, rhs):
+        return self.f < rhs.f
+
+    def __le__(self, rhs):
+        return self.f <= rhs.f
+
+    def __gt__(self, rhs):
+        return self.f > rhs.f
+
+    def __ge__(self, rhs):
+        return self.f >= rhs.f
--- optimize_beta.py
+++ optimize_beta.py
@@ -1,7 +1,9 @@
 from mpmath import mp, mpf, sqrt, pi
 
+from nelder_mead import NelderMead
+
 # Set precision (e.g., 100 decimal places)
-mp.dps = 50
+mp.dps = 500
 
 # Set tighter tolerance
 tolerance = mpf('1e-35')
@@ -11,7 +13,13 @@ c = mpf('299792458.0')          # [m/s] - speed of light
 k = c**2                        # dynamic viscosity [m^2/s]
 h = mpf('6.62607015e-34')       # Planck's constant [J-s], [kg-m^2/s]
 m = h / k                       # elemental mass
-e = mpf('1.602176634e-19')      # Elementary charge (C)
+q = mpf('1.602176634e-19')      # Elementary charge (C)
+k_air = mpf('1.00058986')
+e = q  # * sqrt(k_air)                  # Elementary charge (C)
+
+print(f"e: {float(e)}")
+
+
 eta = mpf('1') / (4 * pi * mpf('1e-7'))  # viscosity [Pa-s], [kg/m-s], [N-s/m^2], [J-s/m^3]
 alpha_codata = mpf('0.007297352569311') # fine structure constant
 
@@ -19,7 +27,13 @@ alpha_codata = mpf('0.007297352569311') # fine structure constant
 a_e  = mpf('0.00115965218059')    # electron magnetic moment anomaly
 a_mu = mpf('0.00116592059')       # muon magnetic moment anomaly
 
+# \[
+#   1 + \beta = \sqrt{1 - \beta} \cdot \sqrt{1 + \beta}
+# \]
 
+#  1 + beta = sqrt(1 - beta) * sqrt(1 + beta)
+
+# alpha_stowe = 1/(2*sqrt(3/k_air)*(2*pi*mma)**2)
 
 def Alpha(beta):
     #return sqrt(1-beta**2) / (8 * pi**2 * sqrt(3))
@@ -29,9 +43,12 @@ def Alpha(beta):
 
     # with this, the initial value for beta results in alpha being very close to the CODATA value
     #return 1 / (8 * pi**2 * sqrt(3) * (1 + beta))
+    #return 1 / (8 * pi**2 * sqrt(3/k_air) * (1 + beta))
 
     # with this, the optimized value for beta results in alpha being very close to the CODATA value
     return 1 / (8 * pi**2 * sqrt(3) * (1 + beta)/sqrt(1-beta**2) )
+
+    #return 1 / (8 * pi**2 * sqrt(3/k_air) * (1 + beta)/sqrt(1-beta**2) )
 
 
 def base1(beta):
@@ -45,6 +62,7 @@ def base2(beta):
     #return (m * c) / (4 * pi**3 * sqrt(3) * (1 + sqrt(beta)) )
     #return (m * c) / (4 * pi**3 * sqrt(3) * (1 + beta) )
     return (m * c) / (4 * pi**3 * sqrt(3) * sqrt(1 + beta) )
+    #return (m * c) / (4 * pi**3 * sqrt(3/k_air) * sqrt(1 + beta) )
 
 def R_1(beta):
     return e / base1(beta)
@@ -60,14 +78,115 @@ def Q2(beta, R):
 
 
 def radii_difference(beta):
+    #print(f"beta: {(beta)}", end="")
+    #print(f"beta: {float(beta)}", end="")
+    #ret = R_1(beta) / R_2(beta) - 1
+    #print(f", ret: {float(ret)}") 
+    #return ret
     return (R_1(beta) - R_2(beta))
+
+def q_term_difference(beta,R):
+    return Q1(beta,R) - Q2(beta,R)
+
+
+def nelder_mead_with_bounds(f, simplex, bounds, tol=mpf('1e-50'), max_iter=10000):
+    """
+    Nelder-Mead optimization with boundary constraints.
+    
+    f: Objective function to minimize.
+    simplex: List of initial vertices (list of mpf arrays).
+    bounds: List of tuples [(lower_bound, upper_bound)] for each parameter.
+    tol: Convergence tolerance.
+    max_iter: Maximum iterations.
+    """
+    n = len(simplex) - 1  # Dimensionality
+    alpha, gamma, rho, sigma = mpf('1'), mpf('2'), mpf('0.5'), mpf('0.5')
+
+    def project_to_bounds(point):
+        """
+        Projects a point to the feasible region defined by bounds.
+        """
+        return [
+            max(bounds[j][0], min(bounds[j][1], point[j]))
+            for j in range(len(point))
+        ]
+
+    for i in range(max_iter):
+        # Sort simplex by function values
+        simplex = sorted(simplex, key=f)
+
+        # Centroid of all but the worst point
+        centroid = [
+            sum(vertex[j] for vertex in simplex[:-1]) / n
+            for j in range(len(simplex[0]))
+        ]
+        worst = simplex[-1]
+
+        # Reflection
+        reflected = [
+            centroid[j] + alpha * (centroid[j] - worst[j])
+            for j in range(len(centroid))
+        ]
+        reflected = project_to_bounds(reflected)
+        if f(simplex[0]) <= f(reflected) < f(simplex[-2]):
+            simplex[-1] = reflected
+            continue
+
+        # Expansion
+        if f(reflected) < f(simplex[0]):
+            expanded = [
+                centroid[j] + gamma * (reflected[j] - centroid[j])
+                for j in range(len(centroid))
+            ]
+            expanded = project_to_bounds(expanded)
+            simplex[-1] = expanded if f(expanded) < f(reflected) else reflected
+            continue
+
+        # Contraction
+        contracted = [
+            centroid[j] + rho * (worst[j] - centroid[j])
+            for j in range(len(centroid))
+        ]
+        contracted = project_to_bounds(contracted)
+        if f(contracted) < f(worst):
+            simplex[-1] = contracted
+        else:
+            # Shrink: Move all points towards the best
+            best = simplex[0]
+            simplex = [best] + [
+                project_to_bounds([
+                    best[j] + sigma * (vertex[j] - best[j])
+                    for j in range(len(best))
+                ])
+                for vertex in simplex[1:]
+            ]
+
+        # Convergence check: Objective function values
+        f_values = [f(vertex) for vertex in simplex]
+        if max(abs(f_val) for f_val in f_values) < tol:
+            print(f"Converged after {i} iterations on f_values with tol={tol}.")
+            print(f"Function values: {f_values}")
+            break
+
+        # Optional: Convergence check based on simplex geometry
+        #max_distance = max(
+        #    sum((vertex1[j] - vertex2[j]) ** 2 for j in range(len(vertex1))) ** 0.5
+        #    for vertex1 in simplex
+        #    for vertex2 in simplex
+        #)
+        #if max_distance < tol:
+        #    print(f"Converged after {i} iterations on max_distance.")
+        #    break
+
+    return simplex[0]  # Return the best point
+
 
 def find_bounds(func, initial, step_factor=mpf('1.001'), max_iter=10000):
     lower_bound = initial
     upper_bound = initial
-    initial_val = radii_difference(initial)
-    lower_val = radii_difference(lower_bound)
-    upper_val = radii_difference(upper_bound)
+    initial_val = func(initial)
+    lower_val = func(lower_bound)
+    upper_val = func(upper_bound)
 
     for iter in range(max_iter):
         if lower_val * upper_val < mpf('0.0'):
@@ -80,8 +199,10 @@ def find_bounds(func, initial, step_factor=mpf('1.001'), max_iter=10000):
         lower_bound /= step_factor
         upper_bound *= step_factor
 
-        lower_val = radii_difference(lower_bound)
-        upper_val = radii_difference(upper_bound)
+        lower_val = func(lower_bound)
+        upper_val = func(upper_bound)
+
+        #print(f"iter: {iter}, lower: {float(lower_bound)}, upper: {float(upper_bound)}, initial: {float(initial)}") 
 
     raise ValueError("Failed to find bounds where the function changes sign.")
 
@@ -158,9 +279,12 @@ print(f"alpha_codata / alpha    : {float(alpha_codata / alpha)}")
 
 # Find the bounds
 lower_bound, upper_bound = find_bounds(radii_difference, initial_beta)
+#lower_bound, upper_bound = find_bounds(q_term_difference, initial_beta)
 
 # Run interval halving
 optimized_beta, min_difference = interval_halving(radii_difference, lower_bound, upper_bound, tol=tolerance)
+#optimized_beta, min_difference = interval_halving(q_term_difference, lower_bound, upper_bound, tol=tolerance)
+
 
 # Compute final R1 and R2 with high precision
 R1_optimized = R_1(optimized_beta)
@@ -227,7 +351,7 @@ print(f"a_mu + 1                : {float(a_mu + 1)}")
 # 1.00058986±0.00000050 (at STP, 900 kHz), 
 # https://doi.org/10.1063%2F1.1745374
 # "A dielectric constant of 1.00058986±0.00000050 is found for air at normal pressure and temperature"
-k_air = mpf('1.00058986')
+#k_air = mpf('1.00058986')
 
 alpha_stowe = 1/(2*sqrt(3/k_air)*(2*pi*mma)**2)
 
@@ -235,3 +359,84 @@ print(f"alpha_stowe             : {float(alpha_stowe)}")
 print(f"alpha_stowe / alpha_codata: {float(alpha_stowe / alpha_codata)}")
 print(f"alpha_codata / alpha_stowe: {float(alpha_codata / alpha_stowe)}")
 
+# Example usage
+def objective(x):
+    #return x[0]**2 + x[1]**2
+    return radii_difference(x[0])
+
+"""
+
+# Define bounds
+#factor = mpf('4')
+#bounds = [(initial_beta / factor, initial_beta * factor)]
+bounds = [(lower_bound, upper_bound)]
+
+factor = mpf('1.01')
+# Define the initial simplex
+#simplex = [
+#    [initial_beta],
+#    [initial_beta * factor],
+#    [initial_beta / factor],
+#]
+
+simplex = [
+    [optimized_beta],
+    [optimized_beta * factor],
+    [optimized_beta / factor],
+]
+
+
+# Run the optimization with bounds
+result = nelder_mead_with_bounds(objective, simplex, bounds)
+
+#print simplex
+print("Initial simplex:", simplex)
+
+
+print("Optimized point:", result)
+print("Function value at optimized point:", objective(result))
+
+print("Optimized beta / result:", optimized_beta / result[0])
+
+func = sphere
+params = {
+    "x1": ["real", (-512, 512)],
+    "x2": ["real", (-512, 512)],
+}
+
+"""
+
+func = objective
+params = {
+    "beta": ["mpf", (lower_bound, upper_bound)],
+}
+
+nm = NelderMead(func, params)
+nm.minimize(n_iter=3000)
+
+optimized_beta = nm.simplex[0].x[0]
+
+
+
+# Print results
+print(f"Optimized beta          : {float(optimized_beta)}")
+print(f"Initial beta / Optimized: {float(initial_beta / optimized_beta)}")
+print(f"1 / sqrt(1 - beta)      : {float(1 / sqrt(1 - optimized_beta))}")
+print(f"1/sqrt(1 + beta)        : {float(1 / sqrt(1 + optimized_beta))}")
+
+print(f"R1 (optimized)          : {float(R1_optimized)}")
+print(f"R2 (optimized)          : {float(R2_optimized)}")
+print(f"R2/R1                   : {float(R2_optimized / R1_optimized)}")
+
+q1 = Q1(optimized_beta, R1_optimized)
+q2 = Q2(optimized_beta, R2_optimized)
+
+print(f"q1                      : {float(q1)}")
+print(f"q2                      : {float(q2)}")
+qratio = q1 / q2
+print(f"q1/q2                   : {float(qratio)}")
+print(f"q1/e                    : {float(q1 / e)}")
+
+
+vcmb = sqrt(optimized_beta**2 * c**2)
+print(f"vcmb                    : {float(vcmb / 1000)} km/s")
